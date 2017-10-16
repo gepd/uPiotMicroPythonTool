36bb1c07592946c3033f92204bbc9bb1f2d86462
gepd <guillermoepd@hotmail.com>
2017-10-15 21:34:53 -0300

    Improved download firmware command, it has been changed to a standalone function, download status will be shown in the status bar

---
+++ commands/download_firmware.py
@@ -0,0 +1,45 @@
+import sublime
+from sublime_plugin import WindowCommand
+
+from ..tools import paths
+from .. import tools
+
+setting_key = 'firmware_url'
+
+
+class upiotDownloadFirmwareCommand(WindowCommand):
+    url = None
+
+    def run(self):
+        self.window.show_input_panel('URL:', '', self.callback, None, None)
+
+    def callback(self, url):
+        """Firmware url
+
+        Gets the url entered by the user and run the download method
+        in a new thread
+
+        Arguments:
+            url {str} -- firmware url
+        """
+        self.url = url
+        sublime.set_timeout_async(self.download_firmware, 0)
+
+    def download_firmware(self):
+        """Download firmware
+
+        If the file isn't in the firmwares folder, it download the file and
+        put it in a folder corresponding to the board selection
+        """
+        folder = paths.firmware_folder('esp32')
+        tools.make_folder(folder)
+
+        tools.ACTIVE_VIEW = self.window.active_view()
+        out = tools.download_file(self.url, folder, callback=tools.set_status)
+
+        if(out):
+            tools.set_status('Download success')
+        else:
+            tools.set_status('Error downloading')
+
+        sublime.set_timeout_async(tools.clean_status, 2000)
