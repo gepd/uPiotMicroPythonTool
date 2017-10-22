a5b63d53bebf83c670c8c53dbc9b70b78e906134
gepd <guillermoepd@hotmail.com>
2017-10-22 04:21:29 -0300

    added unloaded commands to the list of commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -3,8 +3,10 @@ from .download_firmware import upiotDownloadFirmwareCommand
 from .select_port import upiotSelectPortCommand
 from .erase_flash import upiotEraseFlashCommand
 from .retrieve_all_files import upiotRetrieveAllFilesCommand
+from .add_project import upiotAddProjectCommand
 from .put_file import upiotPutFileCommand
 from .remove_file import upiotRemoveFileCommand
+from .put_current_file import upiotPutCurrentFileCommand
 from .make_folder import upiotMakeFolderCommand
 from .remove_folder import upiotRemoveFolderCommand
 from .run_current_file import upiotRunCurrentFileCommand
@@ -15,8 +17,9 @@ __all__ = [
     'upiotBurnFirmwareCommand',
     'upiotDownloadFirmwareCommand',
     'upiotSelectPortCommand',
-    'upiotEraseFlashCommand'
+    'upiotEraseFlashCommand',
     'upiotRetrieveAllFilesCommand',
+    'upiotAddProjectCommand',
     'upiotPutFileCommand',
     'upiotRemoveFileCommand',
     'upiotPutCurrentFileCommand',
@@ -24,5 +27,6 @@ __all__ = [
     'upiotRemoveFolderCommand',
     'upiotRunCurrentFileCommand',
     'upiotConsoleCommand',
-    'upiotConsoleWriteCommand'
+    'upiotConsoleWriteCommand',
+
 ]
