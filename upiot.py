c3606a8ef1c60e5b8bcacb2398428b1d1976154a
gepd <guillermoepd@hotmail.com>
2017-10-22 04:12:50 -0300

    Close the serial connection when the console is closed Recover the console session when sublime text is opened

--- upiot.py
+++ upiot.py
@@ -1,4 +1,18 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from .commands import *
+from sublime_plugin import EventListener
+from .tools import serial
+from .tools import recover_console
+
+
+def plugin_loaded():
+    recover_console()
+
+
+class uPiotListener(EventListener):
+
+    def on_close(self, view):
+        port = serial.selected_port()
+        if(port in serial.in_use):
+            serial.serial_dict[port].close()
