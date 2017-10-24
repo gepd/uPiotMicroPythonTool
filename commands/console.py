6aabc9377ec4de75d0b9e70ff8c1c1832e7f5307
gepd <guillermoepd@hotmail.com>
2017-10-24 03:32:59 -0300

    factorizedand improved code with the last changes added more documentation

--- commands/console.py
+++ commands/console.py
@@ -7,8 +7,6 @@ from ..tools import serial
 
 
 class upiotConsoleCommand(WindowCommand):
-    port = None
-    txt = None
 
     def run(self):
         """Open/Stop serial console
@@ -16,49 +14,14 @@ class upiotConsoleCommand(WindowCommand):
         Opens the serial console in a new window or if it's already open,
         closes the connection to this, it wont close the message window.
         """
-        txt = "uPIOT v{0}\nHelp, suggestion or error reports in\n" \
-            "https: www.github.com/gepd/upiot\n===" \
-            "".format(__version__)
-
-        direction = 'self' if(self.check_empty_panel()) else 'down'
-
         # check the port selected
-        self.port = serial.selected_port(request_port=True)
+        port = serial.selected_port(request_port=True)
 
-        if(not self.port):
+        if(not port):
             return
 
         # create and open the console window
-        self.txt = message.Message(txt)
-        if(not self.txt.recover_panel(self.port)):
-            self.txt.create_panel(direction, extra_name=self.port)
-
-        sublime.set_timeout_async(self.wait_console)
+        serial.establish_connection(port)
 
         # opens the console window
         sublime.active_window().run_command('upiot_console_write')
-
-    def check_empty_panel(self):
-        """
-        If there is an empty panel will make it active
-
-        Returns:
-            bool -- True if there is an empty panel false if not
-        """
-        num = self.window.num_groups()
-
-        for n in range(0, num):
-            if(not self.window.views_in_group(n)):
-                self.window.focus_group(n)
-                return True
-        return False
-
-    def wait_console(self):
-        """Open serial console
-
-        Opens a panel (new file) and starts a serial connection in the selected
-        port and show the data received in human readable format.
-        """
-
-        # Serial port connection
-        serial.establish_connection(self.port, self.txt.print)
--- commands/console_write.py
+++ commands/console_write.py
@@ -6,16 +6,27 @@ from ..tools import serial
 
 
 class upiotConsoleWriteCommand(WindowCommand):
+    port = None
 
     def run(self):
         self.window.show_input_panel('>>', '', self.callback, None, None)
 
     def callback(self, data):
+        """Console write callback
 
-        port = serial.selected_port(request_port=True)
-        if(not port):
+        callback called after write any command in the ST input, if there
+        is the 'sampy' prefix, a sampy (ampy) function will be call, otherwise
+        the string enter will be write in the current serial connection
+
+        Arguments:
+            data {str} -- command to run or data to write
+        """
+        # check if there is a port available
+        self.port = serial.selected_port(request_port=True)
+        if(not self.port):
             return
 
+        # run sampy commands
         if(data.startswith('sampy')):
             data = data.split()
             cmd = data[1]
@@ -27,13 +38,28 @@ class upiotConsoleWriteCommand(WindowCommand):
             self.window.run_command('upiot_console_write')
             return
 
-        if(port in serial.in_use):
-            link = serial.serial_dict[port]
-            link.writable(data)
+        # establish a connection if it doesn't exists
+        if(self.port not in serial.in_use):
+            serial.establish_connection(self.port)
 
-            self.window.run_command('upiot_console_write')
+        link = serial.serial_dict[self.port]
+        link.writable(data)
+
+        self.window.run_command('upiot_console_write')
 
     def sampy_commands(self, option, arg=None):
+        """Console commands
+
+        Executes the commands called from the console, this command
+        are called with the 'sampy' prefix
+
+        Arguments:
+            option {str} -- command option (ls, run, get, etc)
+
+        Keyword Arguments:
+            arg {str} -- command argument required in some commands like
+                        the filename (default: {None})
+        """
         from ..tools import sampy_manager
 
         commands = {}
@@ -49,4 +75,9 @@ class upiotConsoleWriteCommand(WindowCommand):
         try:
             commands[option]()
         except:
-            commands[option](arg)
+            try:
+                commands[option](arg)
+            except:
+                from ..tools import message
+                txt = message.open(self.port)
+                txt.print('\n\n>> CommandError: "{}" not found'.format(option))
