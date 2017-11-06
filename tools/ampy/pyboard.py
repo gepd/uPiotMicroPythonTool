gepd/uPiotMicroPythonTool

commit 6ceba526b00f3812b4e64d9ae0f0187cbaaae9a2

Improved 'sampy run', now it will display the output in realtime. It's important specially when the script have an infinite loop.

tools/ampy/pyboard.py
@@ -43,18 +43,6 @@ import time
 in_use = []
 serial_dict = {}
 
-try:
-    stdout = sys.stdout.buffer
-except AttributeError:
-    # Python2 doesn't have buffer attr
-    stdout = sys.stdout
-
-
-def stdout_write_bytes(b):
-    b = b.replace(b"\x04", b"")
-    stdout.write(b)
-    stdout.flush()
-
 
 class PyboardError(BaseException):
     pass
@@ -88,6 +76,7 @@ class TelnetToSerial:
         self.close()
 
     def close(self):
+        self.data_consumer("closed")
         try:
             self.tn.close()
         except:
@@ -128,8 +117,10 @@ class TelnetToSerial:
 
 class Pyboard:
     port = None
+    data_consumer = None
 
-    def __init__(self, device, baudrate=115200, user='micro', password='python', wait=0):
+    def __init__(self, device, baudrate=115200, user='micro', password='python', wait=0, data_consumer=None):
+        self.data_consumer = data_consumer
         if device and device[0].isdigit() and device[-1].isdigit() and device.count('.') == 3:
             # device looks like an IP address
             self.serial = TelnetToSerial(
@@ -170,13 +161,13 @@ class Pyboard:
     def close(self):
         in_use.remove(self.port)
         del serial_dict[self.port]
+
         self.serial.close()
 
-    def read_until(self, min_num_bytes, ending, timeout=10, data_consumer=None):
+    def read_until(self, min_num_bytes, ending, timeout=10):
         frepl = b'Type "help()" for more information'
         data = self.serial.read(min_num_bytes)
-        if data_consumer:
-            data_consumer(data)
+
         timeout_count = 0
         while True:
             if data.endswith(ending):
@@ -188,14 +179,13 @@ class Pyboard:
             elif self.serial.inWaiting() > 0:
                 new_data = self.serial.read(1)
                 data = data + new_data
-                if data_consumer:
-                    data_consumer(new_data)
                 timeout_count = 0
             else:
                 timeout_count += 1
                 if timeout is not None and timeout_count >= 100 * timeout:
                     break
                 time.sleep(0.01)
+
         return data
 
     def enter_raw_repl(self):
@@ -219,13 +209,18 @@ class Pyboard:
         if not data.endswith(b'soft reboot\r\n'):
             print(data)
             raise PyboardError('could not enter raw repl')
+
         # By splitting this into 2 reads, it allows boot.py to print stuff,
         # which will show up after the soft reboot and before the raw REPL.
         # Modification from original pyboard.py below:
-        #   Add a small delay and send Ctrl-C twice after soft reboot to ensure
-        #   any main program loop in main.py is interrupted.
+        # Add a small delay and send Ctrl-C twice after soft reboot to ensure
+        # any main program loop in main.py is interrupted.
+
         time.sleep(0.5)
+
+        # interrupt any running program
         self.serial.write(b'\x03\x03')
+
         # End modification above.
         data = self.read_until(1, b'raw REPL; CTRL-B to exit\r\n')
         if not data.endswith(b'raw REPL; CTRL-B to exit\r\n'):
@@ -233,26 +228,15 @@ class Pyboard:
             raise PyboardError('could not enter raw repl')
 
     def exit_raw_repl(self):
-        self.serial.write(b'\r\x02')  # ctrl-B: enter friendly REPL
+        # ctrl-B: enter friendly REPL
+        self.serial.write(b'\r\x02')
 
-    def follow(self, timeout, data_consumer=None):
-        # wait for normal output
-        data = self.read_until(1, b'\x04', timeout=timeout,
-                               data_consumer=data_consumer)
-        if not data.endswith(b'\x04'):
-            raise PyboardError('timeout waiting for first EOF reception')
-        data = data[:-1]
+    def eval(self, expression):
+        ret = self.exec_('print({})'.format(expression))
+        ret = ret.strip()
+        return ret
 
-        # wait for error output
-        data_err = self.read_until(1, b'\x04', timeout=timeout)
-        if not data_err.endswith(b'\x04'):
-            raise PyboardError('timeout waiting for second EOF reception')
-        data_err = data_err[:-1]
-
-        # return normal and error output
-        return data, data_err
-
-    def exec_raw_no_follow(self, command):
+    def exec_(self, command):
         if isinstance(command, bytes):
             command_bytes = command
         else:
@@ -275,20 +259,25 @@ class Pyboard:
         if data != b'OK':
             raise PyboardError('could not exec command')
 
-    def exec_raw(self, command, timeout=10, data_consumer=None):
-        self.exec_raw_no_follow(command)
-        return self.follow(timeout, data_consumer)
+        # add to lines in the console
+        self.data_consumer('\n\n')
+        # receive data from the serial port
+        self.receive_serial_data()
+        # Receive data after use '\x03'
+        self.receive_serial_data()
 
-    def eval(self, expression):
-        ret = self.exec_('print({})'.format(expression))
-        ret = ret.strip()
-        return ret
+    def receive_serial_data(self):
+        data = b''
 
-    def exec_(self, command):
-        ret, ret_err = self.exec_raw(command)
-        if ret_err:
-            raise PyboardError('exception', ret, ret_err)
-        return ret
+        while(b'\x04' not in data):
+            data += self.serial.read(1)
+
+            # avoid to print '\x04' char
+            if(data.endswith(b'\r\n') and b'\x04' not in data):
+                # normalizes end of line for ST
+                data = data.replace(b'\r\n', b'\n')
+                self.data_consumer(data)
+                data = b''
 
     def execfile(self, filename):
         with open(filename, 'rb') as f:
@@ -299,83 +288,3 @@ class Pyboard:
         t = str(self.eval('pyb.RTC().datetime()'),
                 encoding='utf8')[1:-1].split(', ')
         return int(t[4]) * 3600 + int(t[5]) * 60 + int(t[6])
-
-# in Python2 exec is a keyword so one must use "exec_"
-# but for Python3 we want to provide the nicer version "exec"
-setattr(Pyboard, "exec", Pyboard.exec_)
-
-
-def execfile(filename, device='/dev/ttyACM0', baudrate=115200, user='micro', password='python'):
-    pyb = Pyboard(device, baudrate, user, password)
-    pyb.enter_raw_repl()
-    output = pyb.execfile(filename)
-    stdout_write_bytes(output)
-    pyb.exit_raw_repl()
-    pyb.close()
-
-
-def main():
-    import argparse
-    cmd_parser = argparse.ArgumentParser(
-        description='Run scripts on the pyboard.')
-    cmd_parser.add_argument('--device', default='/dev/ttyACM0',
-                            help='the serial device or the IP address of the pyboard')
-    cmd_parser.add_argument(
-        '-b', '--baudrate', default=115200, help='the baud rate of the serial device')
-    cmd_parser.add_argument('-u', '--user', default='micro',
-                            help='the telnet login username')
-    cmd_parser.add_argument(
-        '-p', '--password', default='python', help='the telnet login password')
-    cmd_parser.add_argument(
-        '-c', '--command', help='program passed in as string')
-    cmd_parser.add_argument('-w', '--wait', default=0, type=int,
-                            help='seconds to wait for USB connected board to become available')
-    cmd_parser.add_argument('--follow', action='store_true',
-                            help='follow the output after running the scripts [default if no scripts given]')
-    cmd_parser.add_argument('files', nargs='*', help='input files')
-    args = cmd_parser.parse_args()
-
-    def execbuffer(buf):
-        try:
-            pyb = Pyboard(args.device, args.baudrate,
-                          args.user, args.password, args.wait)
-            pyb.enter_raw_repl()
-            ret, ret_err = pyb.exec_raw(
-                buf, timeout=None, data_consumer=stdout_write_bytes)
-            pyb.exit_raw_repl()
-            pyb.close()
-        except PyboardError as er:
-            print(er)
-            sys.exit(1)
-        except KeyboardInterrupt:
-            sys.exit(1)
-        if ret_err:
-            stdout_write_bytes(ret_err)
-            sys.exit(1)
-
-    if args.command is not None:
-        execbuffer(args.command.encode('utf-8'))
-    print(args.files)
-    for filename in args.files:
-        with open(filename, 'rb') as f:
-            pyfile = f.read()
-            execbuffer(pyfile)
-
-    if args.follow or (args.command is None and len(args.files) == 0):
-        try:
-            pyb = Pyboard(args.device, args.baudrate,
-                          args.user, args.password, args.wait)
-            ret, ret_err = pyb.follow(
-                timeout=None, data_consumer=stdout_write_bytes)
-            pyb.close()
-        except PyboardError as er:
-            print(er)
-            sys.exit(1)
-        except KeyboardInterrupt:
-            sys.exit(1)
-        if ret_err:
-            stdout_write_bytes(ret_err)
-            sys.exit(1)
-
-if __name__ == "__main__":
-    main()

tools/ampy/files.py
@@ -226,20 +226,11 @@ class Files(object):
                 raise ex
         self._pyboard.exit_raw_repl()
 
-    def run(self, filename, wait_output=True):
-        """Run the provided script and return its output.  If wait_output is True
-        (default) then wait for the script to finish and then print its output,
-        otherwise just run the script and don't wait for any output.
+    def run(self, filename):
+        """Run the provided script and show the output in realtime.
+        If a print callback was provided in the pyboard module, it will be
+        used to print the output instead of print() used by the ST console
         """
         self._pyboard.enter_raw_repl()
-        out = None
-        if wait_output:
-            # Run the file and wait for output to return.
-            out = self._pyboard.execfile(filename)
-        else:
-            # Read the file and run it using lower level pyboard functions that
-            # won't wait for it to finish or return output.
-            with open(filename, 'rb') as infile:
-                self._pyboard.exec_raw_no_follow(infile.read())
+        self._pyboard.execfile(filename)
         self._pyboard.exit_raw_repl()
-        return out

tools/message.py
@@ -82,6 +82,8 @@ class Message:
         """
         self.text_queue_lock.acquire()
         try:
+            if(type(text) == bytes):
+                text = text.decode('utf-8')
             self.text_queue.append(text)
         finally:
             self.text_queue_lock.release()

tools/sampy.py
@@ -36,7 +36,7 @@ _board = None
 class Sampy:
     _board = None
 
-    def __init__(self, port, baudrate=115200):
+    def __init__(self, port, baudrate=115200, data_consumer=None):
         """ampy - Adafruit MicroPython Tool
 
         Ampy is a tool to control MicroPython boards over a serial connection.
@@ -55,7 +55,7 @@ class Sampy:
         # in windows_full_port_name function).
         if platform() == 'windows':
             port = windows_full_port_name(port)
-        _board = pyboard.Pyboard(port, baudrate)
+        _board = pyboard.Pyboard(port, baudrate, data_consumer=data_consumer)
 
     def get(self, remote_file, local_file=None):
         """
@@ -250,7 +250,7 @@ class Sampy:
         """
         # Run the provided file and print its output.
         board_files = files.Files(_board)
-        output = board_files.run(local_file, not no_output)
+        output = board_files.run(local_file)
         if output is not None:
             return output.decode('utf-8')
 

tools/sampy_manager.py
@@ -58,7 +58,7 @@ def start_sampy():
 
     if(port):
         try:
-            return Sampy(port)
+            return Sampy(port, data_consumer=txt.print)
         except pyboard.PyboardError as e:
             from sys import exit
 
@@ -94,29 +94,13 @@ def run_file(filepath):
 
     # print command name
     file = path.basename(filepath)
-    txt.print('\n\n>> Run {}'.format(file))
 
-    try:
-        output = sampy.run(filepath)
-        output = output.replace('\r\n', '\n').rstrip()
-    except FileNotFoundError as e:
-        output = str(e)
-    except pyboard.PyboardError as e:
-        output = ""
-        # get error
-        output = str(e)
+    head = '\n\n>> Run {0}\n\n"ctrl+shift+c" to stop the script.\n---'
+    txt.print(head.format(file))
 
-        try:
-            # converted in tuple and extract the 'Traceback' error
-            output = str(eval(output)[2])[2:-1]
-        except:
-            pass
-        # replace \r\n strings
-        output = output.replace('\\r\\n', '\n')
-        # replace scape slash
-        output = output.replace('\\\'', '\'')
+    sampy.run(filepath)
 
-    txt.print('\n\n' + output)
+    txt.print("\n[done]")
 
     sampy.close()
 
@@ -187,7 +171,7 @@ def get_files(destination):
             with open(filepath, 'w') as file:
                 file.write(sampy.get(filename))
 
-    txt.print('\n\ndone')
+    txt.print("\n[done]")
 
     sampy.close()
 
@@ -227,7 +211,7 @@ def put_file(filepath):
 
     try:
         sampy.put(path.normpath(filepath))
-        output = 'done'
+        output = '[done]'
     except FileNotFoundError as e:
         output = str(e)
 
@@ -253,7 +237,7 @@ def remove_file(filepath):
 
     try:
         sampy.rm(filepath)
-        output = 'done'
+        output = '[done]'
     except RuntimeError as e:
         output = str(e)
 
@@ -278,7 +262,7 @@ def make_folder(folder_name):
 
     try:
         sampy.mkdir(folder_name)
-        output = 'done'
+        output = '[done]'
     except files.DirectoryExistsError as e:
         output = str(e)
 
@@ -303,7 +287,7 @@ def remove_folder(folder_name):
 
     try:
         sampy.rmdir(folder_name)
-        output = 'done'
+        output = '[done]'
     except RuntimeError as e:
         output = str(e)
 

tools/serial.py
@@ -138,8 +138,9 @@ class Serial:
             printer {obj} -- function or method to print the data
         """
         # clean in and out
-        self._serial.flushInput()
-        self._serial.flushOutput()
+        sleep(1.5)
+
+        self.flush()
 
         while(self.is_running()):
             try:
@@ -154,6 +155,14 @@ class Serial:
 
             if(data.strip()):
                 printer(data)
+
+    def flush(self):
+        """Clean input and output
+
+        Cleans the input and output in the connected serial port
+        """
+        self._serial.flushOutput()
+        self._serial.flushInput()
 
     def close(self):
         """Close serial connection

commit 9613ad1041492c371df017389c6dae6f687d0abe

Fix bug make not work all commands except for "sampy run" introduced in https://github.com/gepd/uPiotMicroPythonTool/commit/6ceba526b00f3812b4e64d9ae0f0187cbaaae9a2

tools/ampy/pyboard.py
@@ -231,12 +231,12 @@ class Pyboard:
         # ctrl-B: enter friendly REPL
         self.serial.write(b'\r\x02')
 
-    def eval(self, expression):
+    def eval(self, expression, quiet=False):
         ret = self.exec_('print({})'.format(expression))
         ret = ret.strip()
         return ret
 
-    def exec_(self, command):
+    def exec_(self, command, quiet=True):
         if isinstance(command, bytes):
             command_bytes = command
         else:
@@ -259,15 +259,20 @@ class Pyboard:
         if data != b'OK':
             raise PyboardError('could not exec command')
 
-        # add to lines in the console
-        self.data_consumer('\n\n')
         # receive data from the serial port
-        self.receive_serial_data()
+        out = self.receive_serial_data(quiet)
         # Receive data after use '\x03'
-        self.receive_serial_data()
+        self.receive_serial_data(quiet)
 
-    def receive_serial_data(self):
+        return out
+
+    def receive_serial_data(self, quiet=True):
+        session_data = b''
         data = b''
+
+        # add to lines in the console
+        if(not quiet):
+            self.data_consumer('\n\n')
 
         while(b'\x04' not in data):
             data += self.serial.read(1)
@@ -276,13 +281,16 @@ class Pyboard:
             if(data.endswith(b'\r\n') and b'\x04' not in data):
                 # normalizes end of line for ST
                 data = data.replace(b'\r\n', b'\n')
-                self.data_consumer(data)
+                if(not quiet):
+                    self.data_consumer(data)
+                session_data += data
                 data = b''
+        return session_data
 
     def execfile(self, filename):
         with open(filename, 'rb') as f:
             pyfile = f.read()
-        return self.exec_(pyfile)
+        return self.exec_(pyfile, quiet=False)
 
     def get_time(self):
         t = str(self.eval('pyb.RTC().datetime()'),